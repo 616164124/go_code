831e65910d5bb88e32d68e6508308c773991e2fa
616164124@qq.com <ert123147>
2021-09-06 15:36:42 +0800

    master

---
+++ project01/main/错误处理.go
@@ -0,0 +1,24 @@
+package main
+
+//TODO
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+
+	i := error1(21, 12)
+	fmt.Println(i)
+
+}
+
+func error1(a int, b int) {
+	if a<b {
+		return 11,errors.New("cuowu!!!!")
+	}else {
+		// return 12,errors.New("错误！！！！！")
+	}
+
+	// return 1
+}
